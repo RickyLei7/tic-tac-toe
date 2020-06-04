97ef69d5d81610783132e4dc837a2f1eee4fbf68
RickyLei <rickylei7@gmail.com>
2020-06-03 19:23:46 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>n is {{ n }}</div>
+    <div class="row">
+      <Cell @:click="onClickCell" :n="n"/>
+      <Cell @:click="onClickCell" :n="n"/>
+      <Cell @:click="onClickCell" :n="n"/>
+    </div>
+    <div class="row">
+      <Cell @:click="onClickCell" :n="n"/>
+      <Cell @:click="onClickCell" :n="n"/>
+      <Cell @:click="onClickCell" :n="n"/>
+    </div>
+    <div class="row">
+      <Cell @:click="onClickCell" :n="n"/>
+      <Cell @:click="onClickCell" :n="n"/>
+      <Cell @:click="onClickCell" :n="n"/>
+    </div>
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Cell from "./Cell"
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    components: {Cell},
+    data() {
+      return {n: 0}
+    },
+    methods: {
+      onClickCell() {
+        this.n += 1
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .row {
+    display: flex;
+  }
 </style>
---
+++ src/Cell.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    {{n}}
+    <div class="cell" v-on:click="onClickCell">
+      <template v-if='a'>{{text}}</template>
+      <template v-else></template>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: ['n'],
+    data() {
+      return {a: false, text: ''}
+    },
+    methods: {
+      onClickCell() {
+        if(this.text!==''){return}
+        this.a = true
+        this.text = this.n % 2 === 0 ? 'X' : 'O'
+        this.$emit('click')
+      }
+    }
+  }
+</script>
+
+<style>
+  .cell {
+    border: 1px solid black;
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 80px;
+  }
+</style>
